76cc00e85a04f02545f60ef688614e66e26d4e1e
adriangough <adriangough@yahoo.com>
2017-01-06 17:00:18 +1100

    Initial Commit

---
+++ SwiftCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCalculator
+//
+//  Created by Alien on 6/01/2017.
+//  Copyright © 2017 Alien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
